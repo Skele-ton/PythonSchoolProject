384fc49078deaa197f367c2cbf423fe2222e321b
Skele-ton <nathanielnechevski@gmail.com>
2023-06-07 22:49:00 +0300

    Improved video making. Actually removed LSTM model.

--- sine.py
+++ sine.py
@@ -1,9 +1,16 @@
-import tensorflow as tf
-from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow import keras
+from video_maker import make_video
 
-x = np.linspace(0, 2*np.pi, 100)
+# Constants
+NUMBER_OF_EPOCHS = 10
+FPS = 10
+OUTPUT_VIDEO = "sine_function_video.mp4"
+FRAME_SIZE = (640, 480)  # Width, height
+
+# Generate sine data
+x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
 
 # Define the model architecture
@@ -16,17 +23,38 @@ model = keras.Sequential([
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-for epoch in range(0, 10):
-    model.fit(x, y, epochs=100, verbose=1)  
+for epoch in range(NUMBER_OF_EPOCHS):
+    model.fit(x, y, epochs=100, verbose=1)
     y_pred = model.predict(x)
 
     plt.plot(x, y, label='Original')
     plt.plot(x, y_pred, label='Predicted')
-    plt.xlabel('x')
-    plt.ylabel('y')
     plt.title('Sine Wave Prediction')
     plt.legend()
-    plt.savefig("graphs/plot_" + "{:04d}".format(epoch) + ".png")
+
+    # Save the current plot as an image file
+    plt.savefig(f"plot_{epoch:04d}.png")
     plt.clf()
 
 model.save('sine_model.h5')
+
+make_video(FPS, OUTPUT_VIDEO, FRAME_SIZE, NUMBER_OF_EPOCHS)
+
+# # Create the video
+# fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec
+# video_writer = cv2.VideoWriter(OUTPUT_VIDEO, fourcc, FPS, FRAME_SIZE)
+
+# # Write each frame to the video
+# for epoch in range(NUMBER_OF_EPOCHS):
+#     # Read the frame image
+#     frame_path = f"plot_{epoch:04d}.png"
+#     frame = cv2.imread(frame_path)
+
+#     # Write the frame to the video
+#     video_writer.write(frame)
+
+#     # Remove the frame image file
+#     os.remove(frame_path)
+
+# # Release the VideoWriter and close the video file
+# video_writer.release()
--- video_maker.py
+++ video_maker.py
@@ -1,26 +1,22 @@
 import cv2
 import os
 
-# Path to the directory containing the image files
-image_directory = "/mnt/c/Users/natha/projects/graphs"
 
-# Get the list of image files in the directory
-image_files = sorted([file for file in os.listdir(image_directory) if file.endswith('.png')])
+def make_video(fps, video_name, window_size, epochs):
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec
+    video_writer = cv2.VideoWriter(video_name, fourcc, fps, window_size)
 
-# Define the output video file path and properties
-output_video = "output_video.mp4"
-fps = 10  # Frames per second
-frame_size = (640, 480)  # Width, height
+# Write each frame to the video
+    for epoch in range(epochs):
+        # Read the frame image
+        frame_path = f"sine_plot_{epoch:04d}.png"
+        frame = cv2.imread(frame_path)
 
-# Create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec
-video_writer = cv2.VideoWriter(output_video, fourcc, fps, frame_size)
+        # Write the frame to the video
+        video_writer.write(frame)
 
-# Iterate over the image files and write each frame to the video
-for image_file in image_files:
-    image_path = os.path.join(image_directory, image_file)
-    frame = cv2.imread(image_path)
-    video_writer.write(frame)
+        # Remove the frame image file
+        os.remove(frame_path)
 
 # Release the VideoWriter and close the video file
-video_writer.release()
+    video_writer.release()
